7e507cf65de0c4ff2b5fe2312dea752d9e555d85
ImAvichal <avichal.vora@gmail.com>
2026-04-29 09:16:16 +1000

    Add files via upload

--- pages/_app.js
+++ pages/_app.js
@@ -1,15 +1,18 @@
 import '../styles/globals.css'
 import { AuthProvider } from '../lib/auth'
+import { ThemeProvider } from '../lib/theme'
 import Head from 'next/head'
 
 export default function App({ Component, pageProps }) {
   return (
-    <AuthProvider>
-      <Head>
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <meta name="theme-color" content="#05091A" />
-      </Head>
-      <Component {...pageProps} />
-    </AuthProvider>
+    <ThemeProvider>
+      <AuthProvider>
+        <Head>
+          <meta name="viewport" content="width=device-width, initial-scale=1" />
+          <meta name="theme-color" content="#05091A" />
+        </Head>
+        <Component {...pageProps} />
+      </AuthProvider>
+    </ThemeProvider>
   )
 }
