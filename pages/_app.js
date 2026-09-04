bbb5fbb1b1c05ed387753d3e73899e12bf68fd46
ImAvichal <avichal.vora@gmail.com>
2026-09-05 08:36:11 +1000

    feat: add GA4 measurement for LearningOnline.ai

--- pages/_app.js
+++ pages/_app.js
@@ -4,6 +4,9 @@ import { ThemeProvider } from '../lib/theme'
 import { I18nProvider } from '../lib/i18n'
 import CookieConsent from '../components/CookieConsent'
 import Head from 'next/head'
+import Script from 'next/script'
+
+const GA_MEASUREMENT_ID = 'G-B3E8TW3LN2'
 
 export default function App({ Component, pageProps }) {
   return (
@@ -14,6 +17,18 @@ export default function App({ Component, pageProps }) {
             <meta name="viewport" content="width=device-width, initial-scale=1" />
             <meta name="theme-color" content="#F0F2F8" />
           </Head>
+          <Script
+            src={`https://www.googletagmanager.com/gtag/js?id=${GA_MEASUREMENT_ID}`}
+            strategy="afterInteractive"
+          />
+          <Script id="google-analytics" strategy="afterInteractive">
+            {`
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', '${GA_MEASUREMENT_ID}');
+            `}
+          </Script>
           <Component {...pageProps} />
           <CookieConsent />
         </AuthProvider>
